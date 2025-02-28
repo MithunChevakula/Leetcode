e735e82bc2f568428a0a77de541177da6e650299
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-02-28 11:26:52 +0530

    Time: 30 ms (5.15%) | Memory: 17.8 MB (92.82%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.py
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.py
@@ -1,5 +1,8 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        a=set(nums1)
-        b=set(nums2)
-        return list(a&b)
+        res=[]
+        for i in nums1:
+            for j in nums2:
+                if i==j and i not in res:
+                    res.append(i)
+        return res

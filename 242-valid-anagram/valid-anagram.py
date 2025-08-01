298952bbde4d9397969ead4981545df4e3522a51
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-08-01 20:08:19 +0530

    Time: 7 ms (88.89%) | Memory: 18 MB (49.66%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -2,5 +2,14 @@ from collections import Counter
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
         #return Counter(s)==Counter(t)
-        return sorted(s)==sorted(t)
+        #return sorted(s)==sorted(t)
+        ar1=[0]*26
+        ar2=[0]*26
+        for i in s.lower():
+            ar1[ord(i)-97]+=1
+        for i in t.lower():
+            ar2[ord(i)-97]+=1
+        return ar1==ar2
+
+
         

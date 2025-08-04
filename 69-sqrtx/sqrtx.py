535d4f2ac434c4c33a81d784a10dd61436e3a84a
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-08-04 19:39:54 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (81.38%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -1,6 +1,7 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        a=math.sqrt(x)
-        b=math.floor(a)
-        return b
+        # a=math.sqrt(x)
+        # b=math.floor(a)
+        # return b
+        return int(x**0.5)
         

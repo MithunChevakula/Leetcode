d45bd2908f986d6102902c233762a03d1425fba0
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-08-04 19:47:21 +0530

    Time: 8 ms (22.65%) | Memory: 17.9 MB (32.32%) - LeetSync

--- 69-sqrtx/sqrtx.py
+++ 69-sqrtx/sqrtx.py
@@ -3,5 +3,16 @@ class Solution:
         # a=math.sqrt(x)
         # b=math.floor(a)
         # return b
-        return int(x**0.5)
+        # return int(x**0.5)
+        left,right=1,x
+        while left<=right:
+            mid=(left+right)//2
+            if mid*mid==x:
+                return mid
+            if mid*mid>x:
+                right=mid-1
+            else:
+                left=mid+1
+        return right
+        
         

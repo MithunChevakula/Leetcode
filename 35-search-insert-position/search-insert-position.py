c16c660d4eb612eacc03fa78d3e9b7cbae74e78c
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-08-05 21:27:48 +0530

    Time: 0 ms (100.00%) | Memory: 18.6 MB (25.74%) - LeetSync

--- 35-search-insert-position/search-insert-position.py
+++ 35-search-insert-position/search-insert-position.py
@@ -1,7 +1,19 @@
 class Solution:
     def searchInsert(self, nums: List[int], target: int) -> int:
-        for i in range(len(nums)):
-            if nums[i]>=target:
-                return i
-        return len(nums)
+        # for i in range(len(nums)):
+        #     if nums[i]>=target:
+        #         return i
+        # return len(nums)
+
+        low=0
+        high=len(nums)-1
+        while low<=high:
+            mid=(low+high)//2
+            if nums[mid]==target:
+                return mid
+            elif nums[mid]<target:
+                low=mid+1
+            else:
+                high=mid-1
+        return low
         

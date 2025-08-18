1fa5dcebd45d3ed9f07b6d1ec3d3d0114505bf89
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-08-18 18:25:28 +0530

    Time: 28 ms (69.31%) | Memory: 39.4 MB (38.60%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.py
+++ 234-palindrome-linked-list/palindrome-linked-list.py
@@ -1,19 +1,25 @@
 class Solution:
     def isPalindrome(self, head: Optional[ListNode]) -> bool:
-        slow = fast = head
-        stack = []
+        # slow = fast = head
+        # stack = []
 
-        while fast and fast.next:
-            stack.append(slow.val)
-            slow = slow.next
-            fast = fast.next.next
+        # while fast and fast.next:
+        #     stack.append(slow.val)
+        #     slow = slow.next
+        #     fast = fast.next.next
 
-        if fast:
-            slow = slow.next
+        # if fast:
+        #     slow = slow.next
 
-        while slow:
-            if stack.pop() != slow.val:
-                return False
-            slow = slow.next
+        # while slow:
+        #     if stack.pop() != slow.val:
+        #         return False
+        #     slow = slow.next
 
-        return True
+        # return True
+        temp=head
+        arr=[]
+        while temp:
+            arr.append(temp.val)
+            temp=temp.next
+        return arr==arr[::-1]

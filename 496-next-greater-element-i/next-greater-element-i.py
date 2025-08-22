ec05075ec2af904952d7b8ec2e4377afa6c9d536
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-08-22 19:19:15 +0530

    Time: 2 ms (45.12%) | Memory: 18.2 MB (21.44%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,13 +1,28 @@
 class Solution:
     def nextGreaterElement(self,nums1, nums2):
+        # ans=[]
+        # for num in nums1:
+        #     index=nums2.index(num)
+        #     next_greater=-1
+        #     for i in range(index+1,len(nums2)):
+        #         if nums2[i]>num:
+        #             next_greater=nums2[i]
+        #             break
+        #     ans.append(next_greater)
+        # return ans
+
+        next_greater={}
+        for i in range(len(nums2)):
+            one=nums2[i]
+            for j in range(i+1,len(nums2)):
+                two=nums2[j]
+                if two>one:
+                    next_greater[one]=two
+                    break
+            else:
+                next_greater[one]=-1
+        
         ans=[]
         for num in nums1:
-            index=nums2.index(num)
-            next_greater=-1
-            for i in range(index+1,len(nums2)):
-                if nums2[i]>num:
-                    next_greater=nums2[i]
-                    break
-            ans.append(next_greater)
+            ans.append(next_greater[num])
         return ans
-

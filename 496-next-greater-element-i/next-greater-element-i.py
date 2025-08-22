80eb8247c662be5e9aac70a765983223b2f1af95
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-08-22 19:23:50 +0530

    Time: 0 ms (100.00%) | Memory: 18.2 MB (21.44%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -11,18 +11,30 @@ class Solution:
         #     ans.append(next_greater)
         # return ans
 
-        next_greater={}
-        for i in range(len(nums2)):
-            one=nums2[i]
-            for j in range(i+1,len(nums2)):
-                two=nums2[j]
-                if two>one:
-                    next_greater[one]=two
-                    break
-            else:
-                next_greater[one]=-1
+        # next_greater={}
+        # for i in range(len(nums2)):
+        #     one=nums2[i]
+        #     for j in range(i+1,len(nums2)):
+        #         two=nums2[j]
+        #         if two>one:
+        #             next_greater[one]=two
+        #             break
+        #     else:
+        #         next_greater[one]=-1
         
-        ans=[]
-        for num in nums1:
-            ans.append(next_greater[num])
-        return ans
+        # ans=[]
+        # for num in nums1:
+        #     ans.append(next_greater[num])
+        # return ans
+        
+        next_greater={}
+        stack=[]
+        for num in reversed(nums2):
+            while stack and stack[-1]<=num:
+                stack.pop()
+            if stack :
+                next_greater[num]=stack[-1]
+            else:
+                next_greater[num]=-1
+            stack.append(num)
+        return [next_greater[num] for num in nums1]

c64f207dce37a35ba4bf0958393231acf8b54fd3
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-08-23 19:55:52 +0530

    Time: 3 ms (5.01%) | Memory: 17.8 MB (55.84%) - LeetSync

---
+++ 1552-build-an-array-with-stack-operations/build-an-array-with-stack-operations.py
@@ -0,0 +1,14 @@
+class Solution:
+    def buildArray(self, target: List[int], n: int) -> List[str]:
+        ans=[]
+        index=0
+        for i in range(1,n+1):
+            if i==target[index]:
+                ans.append("Push")
+                index+=1
+            else:
+                ans.append("Push")
+                ans.append("Pop")
+            if index==len(target):
+                break
+        return ans

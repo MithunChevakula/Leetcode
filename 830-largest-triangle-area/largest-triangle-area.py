2d256b40454ed047227146bce993f16d2aaf4ec4
Mithun Chevakula <155737363+MithunChevakula@users.noreply.github.com>
2025-09-27 13:27:45 +0530

    Time: 35 ms (72.18%) | Memory: 18 MB (9.89%) - LeetSync

--- 830-largest-triangle-area/largest-triangle-area.py
+++ 830-largest-triangle-area/largest-triangle-area.py
@@ -1,4 +1,14 @@
 class Solution:
     def largestTriangleArea(self, points: List[List[int]]) -> float:
-        return max(abs(0.5*(x1*(y2-y3)+x2*(y3-y1)+x3*(y1-y2))) for [x1,y1], [x2,y2], [x3,y3] in combinations(points, 3))
+        n=len(points)
+        maxA=0
+        for i in range(n-2):
+            a0, a1=points[i]
+            for j in range(i+1, n-1):
+                b0, b1=points[j]
+                for k in range(j+1, n):
+                    c0, c1=points[k]
+                    area=(b0-a0)*(c1-a1)-(b1-a1)*(c0-a0)
+                    maxA=max(maxA, abs(area))
+        return 0.5*maxA
         
